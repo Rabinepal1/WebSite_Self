b6f5a9669c91aa16b4327fa2729be7c79be39ac2
Rabi Nepal <103638296+Rabinepal1@users.noreply.github.com>
2023-07-24 21:05:28 +0545

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -25,9 +25,9 @@
 
 // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'sagarnepal98@gmail.com',
+    'username' => 'Rabi Nepal',
+    'password' => 'RabiNepal',
     'port' => '587'
   );
 
